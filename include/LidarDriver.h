97fb97606b6e1c11e4d911dc2f5c43493227736a
sergio <papolino10@gmail.com>
2024-11-26 17:01:51 +0100

    primo commit

---
+++ include/LidarDriver.h
@@ -0,0 +1,56 @@
+#ifndef LIDARDRIVER_H
+#define LIDARDRIVER_H
+
+#include <vector>
+#include <deque>
+#include <exception>
+#include <cmath>
+
+using namespace std;
+
+class LidarDriver {
+    private:
+        // Ricordiamo la differenza tra lettura e scansione:
+        // Il termine lettura identifica la singola misurazione di distanza a un dato angolo, mentre il termine
+        // scansione identifica l’insieme di tutte le letture ai diversi angoli, acquisite in sequenza dal sensore.
+        
+        int BUFFER_DIM;   // Numero di scansioni effettive
+        int angolo;       // Angolo totale da spaziare, questo valore è fisso a 180 gradi
+        double ris_ang;   // Angolo che separa le letture consecutive
+        int counter;      // Contatore per tenere traccia dello scan più nuovo -> E' l'indice per la colonna più vecchia
+        int size;         // Contatore per vedere se il buffer è pieno
+        int nLetture;     // Numero di letture
+        deque<vector<double>> tabella;
+
+    public:
+        LidarDriver(int buffer_size, double ang_ris);
+
+        const int get_BUFFER_DIM() const;              // Metodo per ottenere BUFFER_DIM
+        const double get_ris_ang() const;              // Metodo per ottenere ris_ang
+        const int get_counter() const;           // Metodo per ottenere counter
+        const deque<vector<double>>& get_tabella() const;  // Metodo per ottenere 
+        // ---+---
+        // Organizzando le scan in matrice usiamo coordinate x e y per navigare. La coordinata x indica una colonna.
+        // Una colonna comprende tutte le letture. Una colonna è quindi una scan. Fornendo il parametro y possiamo quindi
+        // scegliere quale lettura stampare 
+        void print_lettura(int x, int y); // Metodo per stampare una singola lettura
+        void print_scan(int x); // Metodo per stampare uno scan
+        void print_all_scans(); // Metodo per stampare la matrice con tutti gli scan
+        // void remove_scan(int x); // Rimuove una scan (colonna) a scelta all'indice x
+
+        void new_scan(vector<double>& scan); // Accetta una nuova scansione e lo memorizza nel buffer e sovrascrive
+                                             // la meno recente se il buffer è pieno. La meno recente è ad indice counter
+        vector<double> get_scan();           // Ritorna l'ultima scan
+        void clear_buffer();                 // Elimina tutte le scansioni salvate
+        double get_distance(double target);  // Ritorna la lettura corrispondente a target nella scansione più recente 
+
+
+        void increment_counter();
+        void decrement_counter();
+};
+
+// Overload operatore
+ostream& operator<<(ostream& os, const LidarDriver& d);
+ostream& operator<<(ostream& os, const vector<double>& v);
+
+#endif // LIDARDRIVER_H
---
+++ src/LidarDriver.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include "../include/LidarDriver.h"
+
+using namespace std;
+
+LidarDriver::LidarDriver(int buffer_size, double ang_ris){
+    angolo = 180;
+    BUFFER_DIM = buffer_size;
+    ris_ang = ang_ris;
+    size = 0;
+    counter = 0;
+    nLetture = static_cast<int>(angolo / ris_ang);
+    for (double x = 0; x < BUFFER_DIM; x++){
+        tabella.push_back(vector<double> ());
+    }
+}
+
+const int LidarDriver::get_BUFFER_DIM() const{
+    return BUFFER_DIM;
+}
+
+const double LidarDriver::get_ris_ang() const{
+    return ris_ang;
+}
+
+const int LidarDriver::get_counter() const{
+    return counter;
+}
+
+const deque<vector<double>>& LidarDriver::get_tabella() const{
+    return tabella;
+}
+
+void LidarDriver::print_lettura(int x, int y){
+    if (x >= tabella.size()) throw out_of_range("Eccezione, indice x fuori range");
+    if (y >= nLetture) throw out_of_range("Eccezione, indice y fuori range");
+    cout << "La lettura del valore a coordinate x: " << x << ", y: " << y << " dello scan è " << LidarDriver::tabella.at(x).at(y) << endl;
+}
+
+void LidarDriver::print_scan(int x){
+    if (x >= tabella.size()) throw out_of_range("Eccezione, indice x fuori range");
+    for (int i = 0; i < nLetture; i++) {
+        cout << LidarDriver::tabella.at(x).at(i) << endl;
+    }
+}
+
+void LidarDriver::print_all_scans(){
+    if (size != 0){     // Se le colonne non sono tutte a 0                   
+        for (int y = 0; y < nLetture; y++){
+            for (int x = 0; x < size; x++){
+                cout << tabella.at(x).at(y) << " ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "Il buffer è vuoto, inserire scans" << endl;
+    }
+}
+
+void LidarDriver::new_scan(vector<double>& v){
+    if (v.size() > nLetture) {
+        for (int i = nLetture; i < v.size(); i++) v.pop_back();     // Nuovo scan da inserire ha più letture di quante dovrebbe averne
+    }
+    if (v.size() < nLetture){                                       // Nuovo scan da inserire ha meno letture di quante dobrebbe averne
+        for (int i = v.size(); i < nLetture; i++) v.push_back(0);
+    }
+    tabella[counter] = v;
+    if (size != BUFFER_DIM) size++;
+    increment_counter();
+}
+
+void LidarDriver::clear_buffer(){
+    tabella.erase(tabella.begin(), tabella.end());
+    counter = 0;
+    for (int i = 0; i < BUFFER_DIM; i++) tabella.push_back(vector<double> ());
+}
+
+double LidarDriver::get_distance(double target){
+    double ang = get_ris_ang();
+    if (target == 0) return tabella[counter - 1][target];   // Angolo richiesto è 0, ritorna la lettura
+                                                            // dello scan più recente in posizione 0
+    if (fmod(target, get_ris_ang()) == 0) {                 // Angolo trovato esattamente
+        return tabella[counter - 1][target];
+    }
+    // gestire eccezione caso in cui venga fornito angolo > 180
+    // gestire eccezione caso in cui angolo sia 0
+}
+
+void LidarDriver::increment_counter(){
+    if (counter == BUFFER_DIM - 1) counter = 0;
+    else counter++;
+}
+
+void LidarDriver::decrement_counter() {
+    if (counter != 0) {
+        counter--;
+    } else {
+        counter = BUFFER_DIM - 1;
+    }
+}
+
+ostream& operator<<(ostream& os, const LidarDriver& d){ 
+    const deque<vector<double>>& appoggio = d.get_tabella();
+    int b = d.get_BUFFER_DIM();
+    int c = d.get_counter();
+    if (c != 0) {
+        c--;
+    } else {
+        c = b - 1;
+    }
+    const vector<double>& app = appoggio[c]; // Reference all'ultima colonna salvata (ultima modificata, 
+    os << app;                               // non ultima per indice)
+    return os;
+}
+
+ostream& operator<<(ostream& os, const vector<double>& v){
+    for (int i = 0; i < v.size(); i++){
+        os << v[i] << endl;
+    }
+    return os;
+}
